83f50844a9e3763cf4dcefe732a30128695921e4
Fatima-ezzahra ELKAWKABI <fatizahra.elkawkabi@gmail.com>
2025-05-27 14:30:35 +0100

    Initial Spring Boot backend

---
+++ backend/backend/src/main/java/com/smartcaravane/backend/SmartCaravaneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartcaravane.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCaravaneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCaravaneBackendApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/com/smartcaravane/backend/SmartCaravaneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcaravane.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCaravaneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
